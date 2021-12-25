f1d879247aa9a86a7428d95b0588e18fd58dd769
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2021-12-25 20:11:36 +0100

    Initial Commit

---
+++ Landmarker/LandmarkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkerApp.swift
+//  Landmarker
+//
+//  Created by Federico Imberti on 25/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
