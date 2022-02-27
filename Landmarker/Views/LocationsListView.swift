5e45b5ed36c957c870c787291dc98a5f5f1a81ba
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-02-27 21:24:46 +0100

    Refactoring of LocationsListView
    showLocationButton
    
    Deleted preview
    
    showTextSection
    
    showDirectionsButton

--- Landmarker/Views/LocationsListView.swift
+++ Landmarker/Views/LocationsListView.swift
@@ -17,21 +17,13 @@ struct LocationsListView: View {
     var body: some View {
         
         ScrollView(showsIndicators: false){
+			
             ForEach(locationManager.locations){ location in
-                Button {
-                    locationManager.showLocation(location: location)
-                    
-                    withAnimation(.spring()) {
-                        isLocationListShown = false
-                    }
-                } label: {
-                    listRowView(location: location)
-                        .padding(.vertical, 4)
-                        .background(Color.clear)
-                }
-                .padding(.vertical, 10)
+				showLocationButton(for: location)
+					.padding(.vertical, 10)
 
             }
+			
             .padding()
             
         }
@@ -40,6 +32,21 @@ struct LocationsListView: View {
 }
 
 extension LocationsListView {
+	
+	private func showLocationButton(for location: Landmark) -> some View {
+		Button {
+			locationManager.showLocation(location: location)
+			
+			withAnimation(.spring()) {
+				isLocationListShown = false
+			}
+		} label: {
+			listRowView(location: location)
+				.padding(.vertical, 4)
+				.background(Color.clear)
+		}
+	}
+	
     private func listRowView(location:Landmark) -> some View{
         HStack{
             
@@ -59,30 +66,38 @@ extension LocationsListView {
             .frame(width: 45, height: 45)
             .cornerRadius(10)
             
-            VStack(alignment: .leading) {
-                Text(location.name)
-                    .font(.headline)
-                    .foregroundColor(.primary)
-                
-                Text(location.cityName)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
+            showTextSection(for: location)
+				.frame(maxWidth: .infinity, alignment: .leading)
             
             Spacer()
             
-            Button {
-                LocationsManager.getDirections(to: location)
-            } label: {
-            Image(systemName: "location.fill")
-                .padding(.horizontal)
-                .symbolRenderingMode(.none)
-                .foregroundColor(.primary)
-            }
+            showDirectionsButton(for: location)
         }
 
     }
+	
+	private func showTextSection(for location: Landmark) -> some View{
+		VStack(alignment: .leading) {
+			Text(location.name)
+				.font(.headline)
+				.foregroundColor(.primary)
+			
+			Text(location.cityName)
+				.font(.subheadline)
+				.foregroundColor(.secondary)
+		}
+	}
+	
+	private func showDirectionsButton(for location: Landmark) -> some View {
+		Button {
+			LocationsManager.getDirections(to: location)
+		} label: {
+		Image(systemName: "location.fill")
+			.padding(.horizontal)
+			.symbolRenderingMode(.none)
+			.foregroundColor(.primary)
+		}
+	}
 }
 
 class LocationListViewModel:ObservableObject {
@@ -101,16 +116,4 @@ class LocationListViewModel:ObservableObject {
         .store(in: &cancellables)
     }
     
-}
-
-
-struct LocationsListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ZStack {
-            Color.red.ignoresSafeArea()
-            
-            LocationsListView(isLocationListShown: .constant(true))
-                .environmentObject(LocationsManager())
-        }
-    }
 }
